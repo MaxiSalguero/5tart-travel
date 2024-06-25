033d1a7d5b29c3b7e29b4c6ddd44940d347b867e
MaxiSalguero <salgueromaximiliano8@gmail.com>
2024-06-24 23:05:26 -0300

    first test on deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World, Welcome to 5tart-travel!';
   }
 }
