90855363dd6a1acaa560cf8aef81bdc771c65923
juank132 <juancarlos_castillo@hotmail.com.ar>
2024-07-01 13:25:38 -0300

    agencia detalle

--- src/agency/agency.service.ts
+++ src/agency/agency.service.ts
@@ -4,12 +4,16 @@ import { AgencyRepository } from './agency.repository';
 @Injectable()
 export class AgencyServices {
   constructor(private agencyRepository: AgencyRepository) {}
-
+  
   getAgency() {
     return this.agencyRepository.getAgency();
   }
-
+  
   deleteAgency(id: string) {
     return this.agencyRepository.deleteAgency(id);
+  }
+
+  getByIdAgency(id: string) {
+    return this.agencyRepository.getByIdAgency(id);
   }
 }
