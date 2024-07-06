9cef2ab5edc6715e5ecf0cede0d4cb91fd26ebc6
MaxiSalguero <salgueromaximiliano8@gmail.com>
2024-07-06 13:50:13 -0300

    routes are protected now

---
+++ src/guards/roles.guard.ts
@@ -0,0 +1,35 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    ForbiddenException,
+    Injectable,
+  } from '@nestjs/common';
+  import { Reflector } from '@nestjs/core';
+  import { Role } from 'src/user/role.enum';
+  
+  @Injectable()
+  export class RolesGuard implements CanActivate {
+    constructor(private readonly reflector: Reflector) {}
+  
+    canActivate(context: ExecutionContext): boolean {
+      const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+        context.getHandler(),
+        context.getClass(),
+      ]);
+  
+      const request = context.switchToHttp().getRequest();
+      const user = request.user;
+  
+      const hasRole = () => requiredRoles.some((rol) => user?.role?.includes(rol));
+      const valid = user && user.role && hasRole();
+  
+      if (!valid) {
+        throw new ForbiddenException(
+          'No tiene permisos para acceder a esta ruta',
+        );
+      }
+  
+      return valid;
+    }
+  }
+  
