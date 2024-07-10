9179391fcb9b399edd66440cc19be0384a982ca1
MaxiSalguero <salgueromaximiliano8@gmail.com>
2024-07-10 14:45:17 -0300

    error in google fixed now

--- src/google/google.module.ts
+++ src/google/google.module.ts
@@ -4,9 +4,10 @@ import { GoogleStrategy } from './google.strategy';
 import { UserEntity } from 'src/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { AgencyEntity } from 'src/entities/agency.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
+  imports: [TypeOrmModule.forFeature([UserEntity, AgencyEntity]), AuthModule],
   controllers: [GoogleController],
   providers: [GoogleStrategy]
 })
