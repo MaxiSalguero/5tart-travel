961ee6f8e4e3dc19566a4c41a87d1fedc925c2d8
MDiaz8778 <maurodiaz8778@gmail.com>
2024-07-16 15:31:00 -0300

    Update mails.service.ts

--- src/mails/mails.service.ts
+++ src/mails/mails.service.ts
@@ -325,47 +325,38 @@ export class mailsServices {
       Saludos cordiales,
       El equipo de 5tart Travel`;
   
-      const htmlBody = `
+    const htmlBody = `
       <div style="border: 2px solid #005BBB; padding: 20px; background: #f5f5f5; border-radius: 15px; text-align: center; max-width: 600px; margin: 0 auto; width: 100%; color: black; position: relative;">
-          <p style="font-size: 24px; font-weight: bold; margin-bottom: 10px;">¡Hola, ${username}!</p>
-          <p style="font-size: 18px;">¡Gracias por tu compra con 5tart Travel!</p>
-      
-          <div style="border: 2px solid #005BBB; padding: 20px; background: white; border-radius: 10px; text-align: left; width: fit-content; margin: 0 auto; color: black; box-shadow: 0 4px 8px rgba(0,0,0,0.1); position: relative;">
+    <p style="font-size: 24px; font-weight: bold; margin-bottom: 10px;">¡Hola, ${username}!</p>
+    <p style="font-size: 18px;">¡Gracias por tu compra con 5tart Travel!</p>
 
-    <!-- Fondo con marca de agua -->
-    <div style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(0, 155, 187, 0.1); z-index: 1; pointer-events: none; display: flex; justify-content: center; align-items: center; border-radius: 10px;">
-        <p style="font-size: 24px; font-weight: bold; color: #009BBA; opacity: 0.6; transform: rotate(-30deg);">MercadoPago</p>
-    </div>
-
-    <!-- Contenido principal -->
-    <div style="position: relative; z-index: 2;">
+    <div style="border: 2px solid #005BBB; padding: 20px; background: white; border-radius: 10px; text-align: left; width: fit-content; margin: 0 auto; color: black; box-shadow: 0 4px 8px rgba(0,0,0,0.1); position: relative;">
         <p style="font-size: 18px; font-weight: bold; margin-bottom: 10px;">Detalles de tu compra</p>
         <hr style="border-top: 2px solid #005BBB; margin-top: 10px; margin-bottom: 15px;">
         <p><strong>Agencia:</strong> ${agencyName}</p>
         <p><strong>Tour:</strong> ${tourName}</p>
-        <p><strong>Precio Total:</strong> ${totalPrice}</p>
+        <p><strong>Precio Total:</strong> $ ${totalPrice} </p>
     </div>
+
+    <p style="margin-top: 20px;">Recibirás información adicional por correo electrónico antes de tu viaje.</p>
+    <p>Si tienes alguna pregunta adicional, no dudes en responder a este correo.</p>
+    <p style="margin-top: 20px;">¡Saludos!</p>
+    <p>El Equipo de 5tart Travel</p>
 </div>
 
-          <p style="margin-top: 20px;">Recibirás información adicional por correo electrónico antes de tu viaje.</p>
-          <p>Si tienes alguna pregunta adicional, no dudes en responder a este correo.</p>
-          <p style="margin-top: 20px;">¡Saludos!</p>
-          <p>El Equipo de 5tart Travel</p>
-      </div>
-      
-      <style>
-          @media only screen and (max-width: 600px) {
-              div[style*="border: 2px solid #005BBB; padding: 20px;"] {
-                  padding: 10px !important;
-              }
-              div[style*="text-align: left; width: fit-content;"] {
-                  width: auto !important;
-                  max-width: calc(100% - 40px) !important;
-              }
-          }
-      </style>
-      `;
-      
+<style>
+    @media only screen and (max-width: 600px) {
+        div[style*="border: 2px solid #005BBB; padding: 20px;"] {
+            padding: 10px;
+        }
+        div[style*="text-align: left; width: fit-content;"] {
+            width: auto;
+            max-width: calc(100% - 40px);
+        }
+    }
+</style>
+
+`;
   
     this.logger.log(
       `Enviando correo a ${userEmail} con asunto "${subject}" y texto "${textBody}"`,
